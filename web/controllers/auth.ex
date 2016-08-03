73f3b6ef3ce558956061f7a1f356a7ac6d02c2e3
Vasilis Spilka <vasspilka@gmail.com>
2016-08-03 18:07:30 +0300

    c10 channels

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -33,9 +33,17 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
